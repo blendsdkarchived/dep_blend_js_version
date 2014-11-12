1a61b5ae6de7e076d02b22c61d114cb92368d9ce
Gevik Babakhani <gevik@blendjs.com>
2014-11-12 11:06:10 +0100

    Fixed test runner path and added .gitignore

--- bin/builder/tests/test-runner.js
+++ bin/builder/tests/test-runner.js
@@ -1,7 +1,7 @@
 require('blend-class-system');
 Blend.loadPath = __dirname + '/../src';
 
-require(__dirname + '/../node_modules/blend-class-system/tests/testframework.js');
+require(__dirname + '/../../../node_modules/blend-class-system/tests/testframework.js');
 require(__dirname + '/10000-sanity.js');
 require(__dirname + '/10000-commandline.js');
 require(__dirname + '/10000-initcommand.js');
