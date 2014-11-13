d8f7da7237778e93c31423c0a776fa82e710e66f
Gevik Babakhani <gevik@blendjs.com>
2014-11-13 20:09:50 +0100

    Added better triggering system for both js and non-js files

--- bin/builder/src/Builder/commands/build/webapp/Builder.js
+++ bin/builder/src/Builder/commands/build/webapp/Builder.js
@@ -2,27 +2,58 @@ var fs = require('fs');
 Blend.defineClass('Builder.commands.build.webapp.Builder', {
     extend: 'Builder.commands.build.Base',
     runInternal: function () {
-        var me = this, files = [], numFiles;
+        var me = this, files = [], jsfiles, numFiles,
+                numJsFiles, analyzeOk = null, depList;
+        me.initFileCache();
+        me.initDepAnalyzer();
+
+        files = me.cache.update();
+        numFiles = Object.keys(files).length;
+
+        if (numFiles !== 0) {
+            /**
+             * Some files have been updated
+             */
+            jsfiles = me.cache.getJSFiles();
+            numJsFiles = Object.keys(jsfiles).length;
+
+            if (numJsFiles !== 0) {
+                if (me.depAnalyzer.analyze(files)) {
+                    //depList = me.depAnalyzer.getDependencyList();
+                    analyzeOk = true;
+                } else {
+                    analyzeOk = false;
+                }
+            }
+
+            if (analyzeOk === true) {
+                // done something
+            } else if (analyzeOk === false) {
+                console.error('Unable to build this application due previous errors!');
+            }
+
+            console.log("analyzeOk", analyzeOk);
+
+        }
+
+
+
+
+
+        numFiles = Object.keys(files).length;
+        if (numFiles !== 0) {
+
+        }
+        return true;
+    },
+    initFileCache: function () {
+        var me = this;
         if (!me.cache) {
             me.cache = Blend.create('Builder.cache.Cache', {
                 root: [me.projectFolder],
                 exts: ['.xml', '.ms', me.getIndexTemplateExtension()]
             });
         }
-        me.createDepAnalyzer();
-        files = me.cache.update();
-        numFiles = Object.keys(files).length;
-        if (numFiles !== 0) {
-            if (me.depAnalyzer.analyze(files)) {
-                /**
-                 * We start generating files and building the application is
-                 * analyze when without errors.
-                 */
-            } else {
-                console.error('Unable to build this application due previous errors!');
-            }
-        }
-        return true;
     }
 });
 
