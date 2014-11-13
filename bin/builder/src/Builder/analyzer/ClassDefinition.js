2177f7cd643029fe686001c0883a11f330a3efe4
Gevik Babakhani <gevik@blendjs.com>
2014-11-13 14:20:51 +0100

    Added initial ClassDefinition class

---
+++ bin/builder/src/Builder/analyzer/ClassDefinition.js
@@ -0,0 +1,18 @@
+Blend.defineClass('Builder.analyzer.ClassDefinition', {
+    className: null,
+    classParent: null,
+    classFile: null,
+    mixins: null,
+    requires: null,
+    controllers: null,
+    properties: null,
+    methods: null,
+    /**
+     * Returns the list of all dependencies
+     * @returns {undefined}
+     */
+    getAllDependencies: function () {
+
+    }
+});
+
