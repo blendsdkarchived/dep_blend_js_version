e6b8e5e510d4d8a77139051371a61c5b02687605
Gevik Babakhani <gevik@blendjs.com>
2014-11-13 14:23:56 +0100

    Make sure the runInternal only does work only when a files is actually changed

--- bin/builder/src/Builder/commands/build/webapp/Builder.js
+++ bin/builder/src/Builder/commands/build/webapp/Builder.js
@@ -1,7 +1,24 @@
+var fs = require('fs');
 Blend.defineClass('Builder.commands.build.webapp.Builder', {
     extend: 'Builder.commands.build.Base',
     runInternal: function () {
-        var me = this;
+        var me = this, files = [], numFiles;
+        if (!me.cache) {
+            me.cache = Blend.create('Builder.cache.Cache', {
+                root: [me.projectFolder],
+                exts: ['.xml', '.ms', me.getIndexTemplateExtension()]
+            });
+        }
+        me.createDepAnalyzer();
+        files = me.cache.update();
+        numFiles = Object.keys(files).length;
+        if (numFiles !== 0) {
+            if (me.depAnalyzer.analyze(files)) {
+                console.log('OK');
+            } else {
+                console.log('NO OK');
+            }
+        }
         return true;
     }
 });
