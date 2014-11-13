97cdb4121620ed7d7d30c5c32b6e7364b9c1b1a3
Gevik Babakhani <gevik@blendjs.com>
2014-11-13 11:17:35 +0100

    Refactored to have findClasses method separately

--- bin/builder/src/Builder/analyzer/Dependency.js
+++ bin/builder/src/Builder/analyzer/Dependency.js
@@ -1,13 +1,48 @@
+var path = require('path');
 Blend.defineClass('Builder.analyzer.Dependency', {
+    requires: [
+        'Builder.cache.Cache',
+        'Builder.analyzer.ClassFinder'
+    ],
     cache: null,
     projectConfig: null,
     env: null,
+    classes: null,
     init: function () {
         var me = this;
         me.callParent.apply(me, arguments);
+        me.classes = {};
         me.cache = Blend.create('Builder.cache.Cache', {
-            root: [me.projectFolder],
+            root: [me.env.projectFolder],
             exts: ['.xml', '.ms', me.getIndexTemplateExtension()]
+        });
+    },
+    analyze: function () {
+        var me = this;
+        me.findAllClasses();
+        return true;
+    },
+    /**
+     * Findes all classes based on the latest cache status
+     * @returns {undefined}
+     */
+    findAllClasses: function () {
+        var me = this, finderResult,
+                files = me.cache.update(),
+                classFinder = Blend.create('Builder.analyzer.ClassFinder');
+        Blend.foreach(files, function (hash, file) {
+            if (path.extname(file) === '.js') {
+                finderResult = classFinder.find(file);
+                if (finderResult.success === true) {
+                    Blend.foreach(finderResult.classes, function (item, className) {
+                        if (me.classes[className]) {
+                            Logger.warn(className + ' already exists in ' + me.classes[className].file + ', skipping.')
+                        } else {
+                            me.classes[className] = item;
+                        }
+                    });
+                }
+            }
         });
     },
     /**
