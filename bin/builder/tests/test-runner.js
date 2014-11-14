087166de22d1fc31ddecabe4a4eb1c19a0555741
Gevik Babakhani <gevik@blendjs.com>
2014-11-14 19:38:57 +0100

    Removed deprecated test

--- bin/builder/tests/test-runner.js
+++ bin/builder/tests/test-runner.js
@@ -5,8 +5,5 @@ require(__dirname + '/../../../node_modules/blend-class-system/tests/testframewo
 require(__dirname + '/10000-sanity.js');
 require(__dirname + '/10000-commandline.js');
 require(__dirname + '/10000-initcommand.js');
-require(__dirname + '/10000-projectconfig.js');
-//require(__dirname + '/10000-cache.js');
-
 
 process.exit(BlendTest.run());
