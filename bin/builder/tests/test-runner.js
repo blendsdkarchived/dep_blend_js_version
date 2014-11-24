afcafe8fc5495a155a09dc3ea2aca7353ff9b6da
Gevik Babakhani <gevik@blendjs.com>
2014-11-25 00:04:21 +0100

    Removed deprecated tests

--- bin/builder/tests/test-runner.js
+++ bin/builder/tests/test-runner.js
@@ -4,6 +4,6 @@ Blend.loadPath = __dirname + '/../src';
 require(__dirname + '/../../../node_modules/blend-class-system/tests/testframework.js');
 require(__dirname + '/10000-sanity.js');
 require(__dirname + '/10000-commandline.js');
-require(__dirname + '/10000-initcommand.js');
+//require(__dirname + '/10000-initcommand.js');
 
 process.exit(BlendTest.run());
