a0c47b6a1fcb01afea8d02b26a008626f05f8b30
Gevik Babakhani <gevik@blendjs.com>
2015-01-13 16:12:22 +0100

    Initial implementation or the base View

---
+++ src/js/ui/View.js
@@ -0,0 +1,41 @@
+Blend.defineClass('Blend.ui.View', {
+    extend: 'Blend.mvc.View',
+    element: null,
+    getElement: function () {
+        var me = this;
+        if (!me._rendered) {
+            me.render();
+        }
+        return me.element;
+    },
+    render: function () {
+        var me = this,
+                el = me.initElement(me.element);
+        el = Blend.Element.create(el, function (oid, element) {
+            /**
+             * Check if we can find a setter for the oid and if possible assign
+             * the element using the setter.
+             */
+            var setterName = 'set' + Blend.camelCase(oid);
+            if (Blend.isFunction(me[setterName])) {
+                me[setterName].apply(me, [element]);
+            } else {
+                me[oid] = element;
+            }
+        }, me);
+        return (me.element = me.finalizeRender(el));
+    },
+    initElement: function (el) {
+        /**
+         * Ini the element
+         */
+        el = Blend.apply(el || {}, {
+            style: {},
+            cls: []
+        }, false, true);
+        return el;
+    },
+    finalizeRender: function (el) {
+        return el;
+    }
+});
