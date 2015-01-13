f4516af5475fd89a0f37ea5525fff856d2b931b2
Gevik Babakhani <gevik@blendjs.com>
2015-01-13 16:11:30 +0100

    Implemented creation of the main view for the application and startup sequence

--- src/js/mvc/Application.js
+++ src/js/mvc/Application.js
@@ -1,12 +1,14 @@
 Blend.defineClass('Blend.mvc.Application', {
     requires: [
         'Blend.mvc.Controller',
-        'Blend.Environment'
+        'Blend.Environment',
+        'Blend.ui.View',
     ],
     mixins: {
         mvcProvider: 'Blend.mvc.Provider',
         mvcConsumer: 'Blend.mvc.Consumer'
     },
+    mainView: null,
     init: function () {
         var me = this;
         me.callParent.apply(me, arguments);
@@ -14,9 +16,39 @@ Blend.defineClass('Blend.mvc.Application', {
         me.mixins.mvcProvider.init.apply(me, arguments);
         me.mixins.mvcConsumer.init.apply(me, arguments);
     },
+    getMainView: function () {
+        var me = this;
+        return me.mainView;
+    },
+    setMainView: function (viewcfg) {
+        var me = this, body = Blend.getBody();
+        me.mainView = me.createView(viewcfg);
+        Blend.Element.clear(body);
+        body.appendChild(me.mainView.getElement());
+    },
+    createView: function (viewConfig) {
+        var me = this, cfg = {
+            mvcContextId: me.getContextId()
+        };
+        if (Blend.isInstanceOf(viewConfig, Blend.ui.View)) {
+            return viewConfig;
+        } else if (Blend.isString(viewConfig)) {
+            return Blend.create(viewConfig, cfg);
+        } else if (Blend.isObject(viewConfig)) {
+            return Blend.create(Blend.apply(cfg, viewConfig));
+        } else {
+            throw new Error('Unable to create a view based on: ' + viewConfig);
+        }
+    },
     run: function () {
         var me = this;
-        me.notifyReady();
+        me.setMainView(me.mainView || 'Blend.ui.View');
+    },
+    start: function () {
+        var me = this;
+        if (me.run() !== false) {
+            me.notifyReady();
+        }
     },
     notifyReady: function () {
         var me = this;
