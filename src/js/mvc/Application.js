1c67b5e2e20e07f963159b3e394280df86e9c0ac
Gevik Babakhani <gevik@blendjs.com>
2015-01-13 20:21:03 +0100

    Implemented event handling when the window page is resized

--- src/js/mvc/Application.js
+++ src/js/mvc/Application.js
@@ -2,6 +2,7 @@ Blend.defineClass('Blend.mvc.Application', {
     requires: [
         'Blend.mvc.Controller',
         'Blend.Environment',
+        'Blend.layout.Util',
         'Blend.ui.View',
     ],
     mixins: {
@@ -20,11 +21,16 @@ Blend.defineClass('Blend.mvc.Application', {
         var me = this;
         return me.mainView;
     },
+    /**
+     * Set the main view for this application.
+     * @param {object/string} viewcfg
+     */
     setMainView: function (viewcfg) {
         var me = this, body = Blend.getBody();
         me.mainView = me.createView(viewcfg);
         Blend.Element.clear(body);
         body.appendChild(me.mainView.getElement());
+        me.layoutMainView();
     },
     createView: function (viewConfig) {
         var me = this, cfg = {
@@ -40,6 +46,42 @@ Blend.defineClass('Blend.mvc.Application', {
             throw new Error('Unable to create a view based on: ' + viewConfig);
         }
     },
+    /**
+     * @private
+     * Layout the mainView by fitting it into the BODY element. The BODY element
+     * already takes the whole page, this means that our mainView will be in full
+     * page when the layout is complete.
+     * @returns {undefined}
+     */
+    layoutMainView: function () {
+        var me = this;
+        Blend.LayoutUtil.fit(Blend.getBody(), me.mainView.getElement());
+    },
+    /**
+     * @private
+     * Hanlder for when the window is being resized.
+     */
+    onWindowResize: function () {
+        var me = this;
+        if (!me._resizing) {
+            me._resizing = true;
+            me.layoutMainView();
+            me._resizing = false;
+        }
+    },
+    /**
+     * @private
+     * Setup a window listener so we can resize the main view and check for the
+     * responsive events.
+     */
+    setupWindowListeners: function () {
+        var me = this;
+        Blend.Environment.addEventListener(window, 'resize', function () {
+            setTimeout(function () {
+                me.onWindowResize.apply(me, arguments);
+            }, 100);
+        });
+    },
     run: function () {
         var me = this;
         me.setMainView(me.mainView || 'Blend.ui.View');
@@ -47,6 +89,7 @@ Blend.defineClass('Blend.mvc.Application', {
     start: function () {
         var me = this;
         if (me.run() !== false) {
+            me.setupWindowListeners();
             me.notifyReady();
         }
     },
