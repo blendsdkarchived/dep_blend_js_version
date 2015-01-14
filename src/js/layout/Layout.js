566a9f0789ab237dbbc178f17f15150129eaf483
Gevik Babakhani <gevik@blendjs.com>
2015-01-14 20:56:14 +0100

    Added base layout class

---
+++ src/js/layout/Layout.js
@@ -0,0 +1,25 @@
+Blend.defineClass('Blend.layout.Layout', {
+    alias: 'layout.base',
+    view: null,
+    performLayout: function () {
+        var me = this;
+        console.log(me.view.$className$);
+    },
+    statics: {
+        createLayout: function (view, layoutConfig) {
+            var me = this, cfg = {
+                view: view
+            };
+            if (Blend.isInstanceOf(layoutConfig, Blend.layout.Layout)) {
+                return layoutConfig;
+            } else if (Blend.isString(layoutConfig)) {
+                layoutConfig = Blend.isAliasOfType('layout', layoutConfig) ? 'layout.' + layoutConfig : layoutConfig;
+                return Blend.create(layoutConfig, cfg);
+            } else if (Blend.isObject(layoutConfig)) {
+                return Blend.create(Blend.apply(cfg, layoutConfig));
+            } else {
+                throw new Error('Unable to create a layout based on: ' + layoutConfig);
+            }
+        }
+    }
+});
