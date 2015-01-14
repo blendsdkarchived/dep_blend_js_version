fd1b7a23d22bedf907ec0d729e3578fe13a1fb7e
Gevik Babakhani <gevik@blendjs.com>
2015-01-14 20:58:27 +0100

    Implemented initial container layout lifecycle

--- src/js/ui/Container.js
+++ src/js/ui/Container.js
@@ -1,11 +1,18 @@
 Blend.defineClass('Blend.ui.Container', {
     extend: 'Blend.ui.Component',
     alias: 'ui.container',
+    layout: 'default',
+    requires: [
+        'Blend.layout.container.Default'
+    ],
     items: null,
     init: function () {
         var me = this;
         me.callParent.apply(me, arguments);
         me.items = Blend.wrapInArray(me.items || []);
+        if (!Blend.isInstanceOf(me.layout, Blend.layout.container.Layout)) {
+            throw new Error('Invalid layout type provided for this container. A container layout type was expected!');
+        }
     },
     initElement: function (el) {
         var me = this;
@@ -14,16 +21,23 @@ Blend.defineClass('Blend.ui.Container', {
         me.createBodyElement(el);
         return el;
     },
-    renderItems: function () {
-        var me = this, items = [], view;
+    createChildViews: function () {
+        var me = this, view;
         Blend.foreach(me.items, function (item, idx) {
             view = me.createChildView(item);
             me.items[idx] = view;
+        });
+        me.items = me.layout.processChildViews(me.items);
+    },
+    renderItems: function () {
+        var me = this, items = [];
+        me.createChildViews();
+        Blend.foreach(me.items, function (view, idx) {
             items.push(view.getElement({
                 cls: [Blend.cssPrefix('conatiner-item')]
             }));
         });
-        return items;
+        return me.layout.processChildElements(items);
     },
     createChildView: function (viewCfg) {
         var me = this;
@@ -39,6 +53,7 @@ Blend.defineClass('Blend.ui.Container', {
     },
     finalizeRender: function (el) {
         var me = this;
+        me.layout.processViewElement(me.bodyEl);
         return el;
     }
 });
