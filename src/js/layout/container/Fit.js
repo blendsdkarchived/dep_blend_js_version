539f65e7059299a77a5aaef4e140414206f2816c
Gevik Babakhani <gevik@blendjs.com>
2015-01-20 11:15:17 +0100

    Updated to layout its visible child view

--- src/js/layout/container/Fit.js
+++ src/js/layout/container/Fit.js
@@ -1,17 +1,21 @@
 Blend.defineClass('Blend.layout.container.Fit', {
     extend: 'Blend.layout.container.Layout',
+    requires: [
+        'Blend.layout.utils.Fit'
+    ],
     alias: 'layout.fit',
     cssPrefix: 'fit',
     getVisibleItemIndex: function () {
         return 0;
     },
-    performLayout: function () {
+    performLayout: function (force) {
         var me = this,
                 ax = me.getVisibleItemIndex();
         Blend.foreach(me.view.items, function (view, idx) {
             if (idx === ax) {
                 view.show();
-                Blend.LayoutUtil.fit(me.containerEl, view.getElement());
+                Blend.layout.utils.Fit.fit(me.containerEl, view.getElement());
+                view.performLayout(force);
             } else {
                 view.hide();
             }
