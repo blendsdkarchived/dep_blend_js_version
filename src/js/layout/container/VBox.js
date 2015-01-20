89836134077016a11234e975dd44e80f61c03d0d
Gevik Babakhani <gevik@blendjs.com>
2015-01-20 12:35:24 +0100

    Removed dead code

--- src/js/layout/container/VBox.js
+++ src/js/layout/container/VBox.js
@@ -2,7 +2,6 @@ Blend.defineClass('Blend.layout.container.VBox', {
     extend: 'Blend.layout.container.Box',
     alias: 'layout.vbox',
     cssPrefix: 'vbox',
-    direction: 'v',
     handler: function () {
         var util = Blend.layout.utils.Box;
         return util.vflex.apply(util, arguments);
