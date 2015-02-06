74cc7c4e77e136b19aa2af9ca23e5312ab49a728
Gevik Babakhani <gevik@blendjs.com>
2015-02-06 21:04:52 +0100

    Refactored installation to the postinstall.js

--- bin/builder/src/Builder/core/Main.js
+++ bin/builder/src/Builder/core/Main.js
@@ -34,32 +34,6 @@ Blend.defineClass('Builder.core.Main', {
         var me = this, command = Blend.create(className, {
             options: options
         });
-        me.prepareCoreFiles();
         command.run();
-    },
-    /**
-     * Copies the core Blend files into the SDK source folder since we do not
-     * ship those seperately in this package
-     * @returns {undefined}
-     */
-    prepareCoreFiles: function () {
-        var me = this, files, sourceFile, targetFile, src, dst,
-                coreFolder = Blend.getSDKFolder('js/core'),
-                bcsFolder = Blend.getRootFolder('node_modules/blend-class-system/lib');
-
-        files = FileUtils.listDir(bcsFolder);
-
-        files.forEach(function (fileName) {
-            sourceFile = Blend.fixPath(bcsFolder + '/' + fileName);
-            targetFile = Blend.fixPath(coreFolder + '/' + fileName);
-            FileUtils.ensurePath(targetFile);
-            FileUtils.writeFile(targetFile, FileUtils.readFile(sourceFile));
-        });
-
-        //copy the Blend.utils.String
-        src = Blend.getRootFolder('bin/builder/src/Builder/utils/String.js');
-        dst = Blend.getSDKFolder('js/utils/String.js');
-        FileUtils.copyFile(src, dst);
     }
-
 });
