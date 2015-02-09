30884ca25225469e687b618154e0e814b9b4a9b0
Gevik Babakhani <gevik@blendjs.com>
2015-02-09 09:58:46 +0100

    Moved theme files to their own folder

--- src/resources/themes/default/blend/config.rb
+++ src/resources/themes/default/blend/config.rb
@@ -1,9 +1,9 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "css"
-sass_dir = "themes"
+http_path = ""
+css_dir = "../../../css/default/blend"
+sass_dir = "."
 images_dir = "images"
 javascripts_dir = "javascripts"
 fonts_dir = "fonts"
