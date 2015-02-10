9ccb1502c2470bf08877c580998cfd00398ad0d6
Gevik Babakhani <gevik@blendjs.com>
2015-02-10 20:01:00 +0100

    Added initial webapp development builder

--- bin/builder/src/Builder/commands/build/webapp/Develop.js
+++ bin/builder/src/Builder/commands/build/webapp/Develop.js
@@ -1,3 +1,26 @@
 Blend.defineClass('Builder.commands.build.webapp.Develop', {
-    extend: 'Builder.commands.build.Builder'
+    extend: 'Builder.commands.build.Builder',
+    _scripts: null,
+    _styles: null,
+    runInternal: function (dmap) {
+        var me = this;
+        me._scripts = me.deployScripts(dmap);
+        me._styles = me.getCSSFiles(dmap);
+        return true;
+    },
+    deployScripts: function (dmap) {
+        var me = this, scripts = me.getScriptFiles(dmap), targetFile, targetName, result = [];
+        Logger.info('Deploying ' + scripts.length + ' scripts, please wait...');
+        Blend.foreach(scripts, function (item) {
+            targetName = 'js/' + item.targetname;
+            targetFile = me.project.getBuildFolder(targetName);
+            FileUtils.copyFile(item.source, targetFile);
+            result.push({src: targetName});
+        });
+        return result;
+    },
+    getIndexTemplateHeader: function () {
+        var me = this;
+        return [me.deployCSSFiles(me._styles), Template.renderScripts(me._scripts)].join("\n");
+    }
 });
