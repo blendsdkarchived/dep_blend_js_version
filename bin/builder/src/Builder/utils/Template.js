4ba4c2c54bd31068b12477e2f051e5d4f5b5304e
Gevik Babakhani <gevik@blendjs.com>
2015-02-10 20:03:52 +0100

    Refactored template

--- bin/builder/src/Builder/utils/Template.js
+++ bin/builder/src/Builder/utils/Template.js
@@ -10,5 +10,61 @@ Blend.defineClass('Template', {
      */
     render: function () {
         return ms.render.apply(ms, arguments);
-    }
+    },
+    renderMetaTags: function (items) {
+        var me = this,
+                template = '        <meta {{{attrs}}}/>',
+                result = [];
+        Blend.foreach(items, function (item) {
+            result.push(ms.render(template, {
+                attrs: me.renderAttrs(item)
+            }));
+        });
+        return result.join("\n");
+    },
+    renderIndex: function (indexTemplateFile, headers) {
+        var content;
+        try {
+            content = FileUtils.readFile(indexTemplateFile).toString();
+            return ms.render(content, headers);
+        } catch (e) {
+            Logger.error(e);
+            return null;
+        }
+    },
+    renderAttrs: function (obj, rewrite) {
+        var kv = [];
+        rewrite = rewrite || function (k) {
+            return k;
+        };
+        Blend.foreach(obj, function (v, k) {
+            k = rewrite(k);
+            kv.push(k + '="' + v + '"');
+        });
+        return kv.join(' ').trim();
+    },
+    renderScripts: function (items) {
+        var me = this,
+                template = '        <script type="text/javascript" src="{{{src}}}"></script>',
+                result = [];
+        Blend.foreach(items, function (item) {
+            result.push(ms.render(template, item));
+        });
+        return result.join("\n");
+    },
+    renderCompactCSS: function (files) {
+        var me = this, content = [],
+                template = '        <style>{{{src}}}</style>';
+        Blend.foreach(files, function (file) {
+            content.push(FileUtils.readFile(file).toString());
+        });
+        return ms.render(template, {src: content.join(';')});
+    },
+    renderScriptSource: function (source) {
+        var me = this,
+                template = '        <script type="text/javascript">{{{src}}}</script>';
+        return ms.render(template, {
+            src: source
+        });
+    },
 });
