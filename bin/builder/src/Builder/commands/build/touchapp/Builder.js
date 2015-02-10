dca8457c7842bf94919f144abcb174f81b246c54
Gevik Babakhani <gevik@blendjs.com>
2015-02-10 20:00:15 +0100

    Removed old files

--- bin/builder/src/Builder/commands/build/touchapp/Builder.js
+++
@@ -1,8 +0,0 @@
-Blend.defineClass('Builder.commands.build.touchapp.Builder', {
-    extend: 'Builder.commands.build.Base',
-    runInternal: function () {
-        var me = this;
-        throw new Error('Builder.commands.build.touchapp.Builder not implemented yet');
-    }
-});
-
