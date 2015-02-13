33106338666c397832502063f3621a2f89468e73
Gevik Babakhani <gevik@blendjs.com>
2015-02-13 17:05:09 +0100

    Implemented bodyPadding configuration option

--- src/js/ui/Container.js
+++ src/js/ui/Container.js
@@ -2,12 +2,16 @@ Blend.defineClass('Blend.ui.Container', {
     extend: 'Blend.ui.AbstractContainer',
     alias: 'ui.container',
     layout: 'default',
+    bodyPadding: 0,
     requires: [
         'Blend.layout.container.Default'
     ],
     initElement: function (el) {
         var me = this;
         Blend.apply(el, {
+            style: {
+                padding: me.bodyPadding
+            },
             items: me.createBodyElement()
         });
         return me.callParent.apply(me, arguments);
