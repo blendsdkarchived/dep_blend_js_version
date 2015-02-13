1a5def84b14b14fdc4cf2811cde3394edbf566d2
Gevik Babakhani <gevik@blendjs.com>
2015-02-13 12:00:23 +0100

    Removed unused variables

--- ui-tests/js/tests/10020-center-layout.js
+++ ui-tests/js/tests/10020-center-layout.js
@@ -9,7 +9,6 @@ BlendTest.defineTest('center-layout', 'center default', function (t) {
             ready: function (app) {
                 var me = this;
                 t.delay(function () {
-                    var r0 = Blend.Element.getSizeAndPosition(me.getRect0().getElement());
                     var r1 = Blend.Element.getSizeAndPosition(me.getRect1().getElement());
                     t.equal(r1.top, 150, 'component top ok');
                     t.equal(r1.left, 150, 'component left ok');
@@ -60,7 +59,6 @@ BlendTest.defineTest('center-layout', 'center with padding', function (t) {
             ready: function (app) {
                 var me = this;
                 t.delay(function () {
-                    var r0 = Blend.Element.getSizeAndPosition(me.getRect0().getElement());
                     var r1 = Blend.Element.getSizeAndPosition(me.getRect1().getElement());
                     t.equal(r1.top, 140, 'component top ok');
                     t.equal(r1.left, 140, 'component left ok');
@@ -110,7 +108,6 @@ BlendTest.defineTest('center-layout', 'center with border and padding', function
             ready: function (app) {
                 var me = this;
                 t.delay(function () {
-                    var r0 = Blend.Element.getSizeAndPosition(me.getRect0().getElement());
                     var r1 = Blend.Element.getSizeAndPosition(me.getRect1().getElement());
                     t.equal(r1.top, 135, 'component top ok');
                     t.equal(r1.left, 135, 'component left ok');
